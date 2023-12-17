cb823e5a0df5450c5eb6c57b86f7623f2e3c8c40
Hanif <hnfsrj@gmail.com>
2023-12-18 02:13:42 +0300

    more chaos

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ let add = document.querySelector('.add');
 
 function adder(){
     for(let i=0;i<=1000;i++){
-        let addnumbers=`<button style='width:${Math.floor(Math.random()*23)+18}px;height:${Math.floor(Math.random()*23)+15}px;' class='numnum' data-status="active">${i}</button>`;
+        let addnumbers=`<button style='width:${Math.floor(Math.random()*23)+18}px;height:${Math.floor(Math.random()*23)+15}px;position:relative;' class='numnum' data-status="active" data-motion="1">${i}</button>`;
         
         mid.insertAdjacentHTML('beforeend',addnumbers);
     }
@@ -52,3 +52,33 @@ bottom.addEventListener('click',function(e){
 });
 
 adder();
+
+function sleep(ms){
+    return new Promise(resolve=>setTimeout(resolve,ms));
+}
+
+async function moveAround(){
+    let numnums = document.querySelectorAll('.numnum');
+
+    while(true){
+        for(let i=0;i<=1000;i++){
+            if(numnums[i].getAttribute('data-motion') == "1"){
+                numnums[i].style.left=`${Math.floor(Math.random()*21)}px`;
+                numnums[i].style.top=`${Math.floor(Math.random()*21)}px`;
+                numnums[i].dataset.motion = "0";
+            }else{
+                numnums[i].style.right=`${Math.floor(Math.random()*21)}px`;
+                numnums[i].style.bottom=`${Math.floor(Math.random()*21)}px`;
+                numnums[i].dataset.motion = "1";
+            }
+
+            numnums[i].style.transform=`rotate(${Math.floor(Math.random()*361)}deg)`;
+            numnums[i].style.background=`rgb(${Math.floor(Math.random()*256)},${Math.floor(Math.random()*256)},${Math.floor(Math.random()*256)})`;
+            mid.style.background=`rgb(${Math.floor(Math.random()*256)},${Math.floor(Math.random()*256)},${Math.floor(Math.random()*256)})`;
+        };
+
+        await sleep(400);
+    };
+};
+
+moveAround();
